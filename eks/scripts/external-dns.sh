d2e9b4063eae57e7949dcf54c5a341abb83faf64
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-04-11 15:42:39 +0530

    update service account creation

--- eks/scripts/external-dns.sh
+++ eks/scripts/external-dns.sh
@@ -10,6 +10,7 @@ eksctl create iamserviceaccount \
   --name "external-dns" \
   --namespace kube-system \
   --attach-policy-arn $POLICY_ARN \
+  --override-existing-serviceaccounts \
   --approve
 
 echo "Add external DNS helm chart"
