cc31f95b427f22431e739ab9816c471244b8d548
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-04-14 07:39:45 +0530

    add the DNS Controller

--- eks/scripts/external-dns.sh
+++ eks/scripts/external-dns.sh
@@ -2,24 +2,26 @@
 
 EKS_CLUSTER_NAME="karpenter"
 POLICY_ARN="arn:aws:iam::522814728660:policy/ExternalDNSPolicy"
-REGION="ap-south-1"
 
-
-echo "Create OIDC for External DNS"
+echo "Creating Service Account"
 eksctl create iamserviceaccount \
-  --cluster $EKS_CLUSTER_NAME \
-  --region $REGION \
-  --name "external-dns" \
-  --namespace kube-system \
-  --attach-policy-arn $POLICY_ARN \
-  --override-existing-serviceaccounts \
-  --approve
+      --cluster=$EKS_CLUSTER_NAME \
+      --namespace=kube-system \
+      --name=external-dns \
+      --attach-policy-arn=$POLICY_ARN \
+      --approve
 
 echo "Add external DNS helm chart"
 helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
 
-echo "Installing DNS helm chart"
-helm upgrade --install external-dns external-dns/external-dns --namespace kube-system
+echo "Install DNS COntroller"
+helm install external-dns external-dns/external-dns \
+    --set serviceAccount.create=false \
+    --set serviceAccount.name=external-dns \
+    -n kube-system
+
+
+
 
 
 
