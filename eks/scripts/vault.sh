602b4ef9ae4863919284a30ae0ac2055e5e2f3f4
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-04-17 11:06:13 +0530

    update the config

--- eks/scripts/vault.sh
+++ eks/scripts/vault.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+echo "Add HV helm repo"
+helm repo add external-secrets https://charts.external-secrets.io
+
+echo "install hv"
+helm install external-secrets \
+   external-secrets/external-secrets \
+    -n kube-system
+
+echo "Install ClusterSecretStore"
+kubectl apply -f ss.yaml  
